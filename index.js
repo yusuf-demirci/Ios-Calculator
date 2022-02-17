9ddf771b6a33989b050ba27aeb2292568ac427cc
Yusuf Demirci <ydemirci7@gmail.com>
2022-02-17 21:54:26 +0300

    small changes

--- index.js
+++ index.js
@@ -19,70 +19,77 @@ function calculate(a, b, operator) {
             result = a / b;
             break;
     }
-    return Math.floor(result * 1000) / 1000
+    return Math.floor(result * 1000) / 1000;
 }
 
 function numClick(content) {
-    if (isOperatorActive) currentNumber.text("0")
+    if (isOperatorActive) currentNumber.text("0");
 
-    if (currentNumber.text().length > 7) currentNumber.css("font-size", "4rem")
-    else currentNumber.css("font-size", "5rem")
+    if ($(window).width() < 361) {
+        if (currentNumber.text().length > 7) currentNumber.css("font-size", "2.5rem");
+        else currentNumber.css("font-size", "3rem");
+    } else if ($(window).width() < 431) {
+        if (currentNumber.text().length > 7) currentNumber.css("font-size", "3.5rem");
+        else currentNumber.css("font-size", "4rem");
+    } else {
+        if (currentNumber.text().length > 7) currentNumber.css("font-size", "4rem");
+        else currentNumber.css("font-size", "5rem");
+    }
 
-    if (currentNumber.text().length > 8) return
-
-    $(".reset").text("C")
+    if (currentNumber.text().length > 8) return;
+    $(".reset").text("C");
 
     if (content === ".") {
-        !currentNumber.text().includes(".") && currentNumber.text(currentNumber.text() + content)
+        !currentNumber.text().includes(".") && currentNumber.text(currentNumber.text() + content);
     }
     else {
-        if (currentNumber.text().includes(".")) {
-            currentNumber.text(currentNumber.text() + content);
-        } else {
-            currentNumber.text(+(currentNumber.text() + content));
-        }
+        if (currentNumber.text().includes(".")) currentNumber.text(currentNumber.text() + content);
+        else currentNumber.text(+(currentNumber.text() + content));
     }
     isOperatorActive = false;
 }
 
 function operatorClick(operator) {
     if (isOperatorActive) {
-        currentOperator = operator
-        memory = currentNumber.text()
-        return
+        currentOperator = operator;
+        memory = currentNumber.text();
+        return;
     }
-    isOperatorActive = true
+    isOperatorActive = true;
 
     if (memory) {
-        currentNumber.text(calculate(+memory, +currentNumber.text(), currentOperator) + "")
+        currentNumber.text(calculate(+memory, +currentNumber.text(), currentOperator) + "");
     }
 
-    memory = currentNumber.text()
-    currentOperator = operator
+    memory = currentNumber.text();
+    currentOperator = operator;
 }
 
 function equalClick() {
     if (isOperatorActive || !memory) return;
-    currentNumber.text(calculate(+memory, +currentNumber.text(), currentOperator) + "")
+    currentNumber.text(calculate(+memory, +currentNumber.text(), currentOperator) + "");
     isOperatorActive = true;
-    currentOperator = ""
+    currentOperator = "";
     memory = null;
 }
 
 function resetClick() {
     currentNumber.text("0");
     memory = null;
-    currentNumber.css("font-size", "5rem")
-    $(".reset").text("AC")
+    $(".reset").text("AC");
+
+    if ($(window).width() < 361) currentNumber.css("font-size", "3rem");
+    else if ($(window).width() < 431) currentNumber.css("font-size", "4rem");
+    else currentNumber.css("font-size", "5rem");
 }
 
 $(".plus-minus").click(() => {
-    if (currentNumber.text() !== "0") currentNumber.text(+currentNumber.text() * -1)
+    if (currentNumber.text() !== "0") currentNumber.text(+currentNumber.text() * -1);
     $(this).blur();
 })
 
 $(".percent").click(() => {
-    currentNumber.text(+currentNumber.text() / 100)
+    currentNumber.text(+currentNumber.text() / 100);
     $(this).blur();
 })
 
@@ -91,26 +98,26 @@ $(".num, .dot").click(function (e) {
     $(this).blur();
 })
 
-$(".reset").click(function(){
+$(".reset").click(function () {
     resetClick();
     $(this).blur();
 })
 
 $(".operator").click(function () {
-    operatorClick($(this).attr("name"))
+    operatorClick($(this).attr("name"));
     $(this).blur();
 })
 
-$(".equal").click(function(){
+$(".equal").click(function () {
     equalClick();
     $(this).blur();
 })
 
 $(document).keydown((e) => {
-    if (!isNaN(e.key) || e.key === ".") numClick(e.key)
-    else if (["+", "-", "*", "/"].includes(e.key)) operatorClick(e.key)
-    else if (e.key === "Enter" || e.key === "NumpadEnter") equalClick()
-    else if (e.key === "Escape") resetClick()
+    if (!isNaN(e.key) || e.key === ".") numClick(e.key);
+    else if (["+", "-", "*", "/"].includes(e.key)) operatorClick(e.key);
+    else if (e.key === "Enter" || e.key === "NumpadEnter") equalClick();
+    else if (e.key === "Escape") resetClick();
 })
 
 
