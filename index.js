3599d5f15a41651a0077d6dc90c9222f68f1840d
Yusuf Demirci <ydemirci7@gmail.com>
2022-02-17 17:18:35 +0300

    index.js updated

--- index.js
+++ index.js
@@ -1,7 +1,74 @@
-$(".num").click((e) => {
-    $(".screen").text(+($(".screen").text() + e.target.textContent));
+let currentNumber = $(".screen");
+let previousNumber = 0;
+let isOperatorActive = false;
+let memory;
+let currentOperator = "";
+
+function calculate(a, b, operator) {
+    switch (operator) {
+        case "+":
+            return (a + b).toFixed(2);
+        case "-":
+            return (a - b).toFixed(2);
+        case "*":
+            return (a * b).toFixed(2);
+        case "/":
+            return (a / b).toFixed(2);
+    }
+}
+
+$(".num, .dot").click((e) => {
+    if (isOperatorActive) currentNumber.text("0")
+
+    if (currentNumber.text().length > 7) currentNumber.css("font-size", "4rem")
+    else currentNumber.css("font-size", "5rem")
+
+    if (e.target.textContent === ".") {
+        !currentNumber.text().includes(".") && currentNumber.text(currentNumber.text() + e.target.textContent)
+    }
+    else {
+        if (currentNumber.text().includes(".")){
+            currentNumber.text(currentNumber.text() + e.target.textContent);
+        } else {
+            currentNumber.text(+(currentNumber.text() + e.target.textContent));
+        }
+    }
+    isOperatorActive = false;
 })
 
 $(".reset").click(() => {
-    $(".screen").text("0");
+    currentNumber.text("0");
+    memory = null;
+    currentNumber.css("font-size", "5rem")
 })
+
+$(".operator").click(function () {
+    if (isOperatorActive) {
+        currentOperator = $(this).attr("name")
+        memory = currentNumber.text()
+        return
+    }
+    isOperatorActive = true
+
+    if (memory) {
+        currentNumber.text(calculate(+memory, +currentNumber.text(), currentOperator) + "")
+    }
+
+    memory = currentNumber.text()
+    currentOperator = $(this).attr("name")
+})
+
+$(".equal").click(() => {
+    if (isOperatorActive || !memory) return;
+
+    currentNumber.text(calculate(+memory, +currentNumber.text(), currentOperator) + "")
+    isOperatorActive = true;
+    currentOperator = ""
+    memory = null;
+})
+
+
+
+
+
+
